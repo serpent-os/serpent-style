42dc9910751644990aeb9e1126607c624438a466
Rune Morling <ermo@serpentos.com>
2021-12-10 11:54:57 +0100

    scripts/update_format.sh: Add basic error checking blocks
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- scripts/update_format.sh
+++ scripts/update_format.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
+#
+# Copyright © 2021 Serpent OS developers
+#
+
+if [[ ! -d .git/ ]]; then
+    echo "ERROR: ${PWD} does not contain a .git/ directory?"
+    echo "       The script needs to be run from the root of a git repository."
+    exit 1
+fi
+
+if [[ ! -d scripts/ ]]; then
+    echo "ERROR: No './scripts/' directory found?"
+    echo "       Please ensure that this script is run from the root of a git repository."
+    exit 1
+fi
+
+if [[ ! -d source/ ]]; then
+    echo "ERROR: No './source/' directory found?"
+    exit 1
+fi
+
+D_FILES=""
 
 # Autoformat the code.
 command -v dfmt 2>&1 > /dev/null
 if [[ $? -eq 0 ]]; then
-    dfmt -i $(find source/ -name '*.d')
+    D_FILES="$(find source/ -name '*.d')"
+    if [[ ${D_FILES} != "" ]]; then
+        dfmt -i "${D_FILES}"
+    else
+        echo "ERROR: No *.d files found in ./source/ ?"
+        exit 1
+    fi
 else
     echo "ERROR: dfmt code formatting tool not found in your PATH?"
     echo "       Please install dfmt and rerun scripts/update_format.sh"
@@ -14,8 +42,7 @@ fi
 # Check we have no typos.
 command -v codespell 2>&1 >/dev/null
 if [[ $? -eq 0 ]]; then
-    echo "Potentially misspelled words (per codespell):"
-    codespell $(find source/ -name '*.d')
+    codespell "${D_FILES}"
 else
     echo "ERROR: codespell not found?"
     echo "       Please install codespell!"
@@ -23,4 +50,4 @@ else
 fi
 
 # Nuke .orig files from modification
-find . -name '*.d.orig' | xargs -I{} rm -v {}
+# find . -name '*.d.orig' | xargs -I{} rm -v {}
