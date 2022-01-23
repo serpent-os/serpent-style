9f910914918c443839c97149127323b37687a9fd
Rune Morling <ermo@serpentos.com>
2022-01-24 00:42:32 +0100

    templates/: Add basic dlang and shell code templates
    Signed-off-by: Rune Morling <ermo@serpentos.com>

---
+++ templates/dlang.d
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * FIXME: Module Name
+ * 
+ * FIXME: Description
+ * 
+ * Authors: FIXME
+ * License: ZLib
+ * Copyright: © 2020-2022 Serpent OS Developers
+ */
+module FIXME;
---
+++ templates/shell.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: Zlib
+#
+# Copyright: © 2020-2022 Serpent OS Developers
+#
+
