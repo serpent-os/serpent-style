cb9649441690e128a24e29615442193544f16470
Rune Morling <ermo@serpentos.com>
2022-03-07 10:48:26 +0100

    pre-commit: Switch to inline gawk script for pattern matches
    This is both more flexible and readable when it comes to adding illegal
    patterns to reject.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- git-pre-commit-hook.sh
+++ git-pre-commit-hook.sh
@@ -36,21 +36,62 @@ function noisyFail ()
     exit 1
 }
 
-# we don't want writeln / writefln in the code
-# (except under specific circumstances)
-
-function forbiddenPatterns ()
+function checkForTools ()
 {
-    # let's start with simple printing patterns
-    local FORBIDDEN='write(f|)ln'
+    local TOOLS=(
+        grep
+        gawk
+    )
+    local MISSING_TOOLS=""
+    echo -e "\nChecking for tools used by serpent-style git pre-commit hook..."
+    for tool in ${TOOLS[@]}; do
+        command -v ${tool} 2>&1 > /dev/null
+        if [[ ! $? -eq 0 ]]; then
+            MISSING_TOOLS+="  ${tool}\n"
+        else
+            echo -e "'- found ${tool}..."
+        fi
+    done
+    if [[ ! -z "${MISSING_TOOLS}" ]]; then
+        noisyFail "
+  Missing tools:
+  ${MISSING_TOOLS}
+  Git pre-commit hook aborted.
+"
+    else
+        echo ""
+    fi
+}
+
+function rejectForbiddenPatterns ()
+{
     git diff --cached --name-only | \
-        grep -E "${D_FILES}" | \
-        GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n -E "${FORBIDDEN}" \
-        && noisyFail "
+    grep -E "${D_FILES}" |grep -v moss-vendor | \
+    xargs gawk -- '
+# we need this for exit status
+BEGIN { matches = 0 }
+
+print "Checking for illegal Dlang patterns...\n" 
+
+# Illegal patterns 
+# only match lines that are not commented out (we use 4 space indents)
+# each line of Dlang code is matched against all the patterns below in the order listed
+
+# disallow writeln and writefln (use debug/info from std.experimental.logger interface instead)
+/^[ ]*write(f|)ln/ { print FILENAME ":" FNR ":" $0 ; matches += 1 }
+
+# disallow buildPath (use join instead)
+/^[ ]*buildPath/ { print FILENAME ":" FNR ":" $0 ; matches += 1 }
+    
+# exit 1 on illegal patterns found
+END { exit (matches != 0) }
+' \
+       || noisyFail "
   COMMIT REJECTED
-  -- Found \$FORBIDDEN ('$FORBIDDEN') references.
+  -- Found illegal Dlang code patterns.
   >> Please remove them before committing!"
 }
 
 # list of enabled functions
-forbiddenPatterns
+checkForTools && rejectForbiddenPatterns
+
