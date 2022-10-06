95509c0660b6ce5340a9762ec1eb8c1e48605cf0
Rune Morling <ermo@serpentos.com>
2022-10-06 16:05:31 +0200

    Tweak git pre-commit hook patterns and output
    Chiefly, this ensures that error reporting is more useful by following
    a "where, what, how-to-fix, why" pattern.
    
    Updated at Ikey's request.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- git-pre-commit-hook.sh
+++ git-pre-commit-hook.sh
@@ -45,8 +45,8 @@ function checkForTools ()
     )
     local MISSING_TOOLS=""
     echo -e "\nChecking for tools used by serpent-style git pre-commit hook..."
-    for tool in ${TOOLS[@]}; do
-        command -v ${tool} 2>&1 > /dev/null
+    for tool in "${TOOLS[@]}"; do
+        command -v "${tool}" > /dev/null 2>&1
         if [[ ! $? -eq 0 ]]; then
             MISSING_TOOLS+="  ${tool}\n"
         else
@@ -64,35 +64,55 @@ function checkForTools ()
     fi
 }
 
-# Ignore logger class code and general cli code, since both need to
-# be able to write{f,}ln.
-# Alse ignore deleted files in 'git diff --cached --name-only'
-# by adding --diff-filter=d 
+# Ignore vendor code, since we only aim to consume it
+#
+# Alse ignore deleted files in 'git diff --cached --name-only' by adding
+# '--diff-filter=d'
+#
 function rejectForbiddenPatterns ()
 {
     git diff --cached --name-only --diff-filter=d | \
         grep -E "${D_FILES}" | \
-        grep -Ev '^D|moss-vendor|moss-core/source/moss/core/logger.d|/cli/' | \
+        grep -Ev '^moss-vendor' | \
         xargs gawk -- '
 # we need this for exit status
-BEGIN { matches = 0 }
+BEGIN { matches = 0; }
+
+function error(msg) {
+    print FILENAME ":" FNR ":";
+    print ">" $0 "<";
+    print ">> " msg "\n";
+    matches += 1;
+}
 
 # Illegal patterns
 # only match lines that are not commented out (we use 4 space indents)
 # each line of Dlang code is matched against all the patterns below in the order listed
+#
+# Use a "where, what, how-to-fix (why)" format for usability
 
-# disallow writeln and writefln (use trace/info/warning etc.
-# from std.experimental.logger interface instead)
-/^[ ]*write(f|)ln/ { print FILENAME ":" FNR ":" $0 ; matches += 1 }
+# disallow writefln run-time format strings
+/^[ ]*writefln\(/ {
+    error("Use writefln! instead of writefln() (compile time format string check)");
+}
 
-# Use trace(format!"string with %s"(substitution)); instead (compile time check)
-/^[ ]*(log|trace|info|warning|error|critical|fatal)f/ { print FILENAME ":" FNR ":" $0 ; matches += 1 }
+# disallow logger run-time format strings
+/^[ ]*(log|trace|info|warning|error|critical|fatal)f/ {
+    error("Use e.g. info(format! instead (compile time format string check)");
+}
 
-# disallow buildPath (use .join instead)
-/^[ ]*buildPath/ { print FILENAME ":" FNR ":" $0 ; matches += 1 }
+# buildPath has been shown to be slow
+/^[ ]*buildPath/ {
+    error("Use .join or joinPath instead of buildPath (speedup)");
+}
 
 # exit 1 on illegal patterns found
-END { exit (matches != 0) }
+END {
+    if (matches != 0) {
+        print "Found " matches " illegal Dlang patterns.";
+        exit (matches != 0)
+    }
+}
 ' \
        || noisyFail "
   COMMIT REJECTED
