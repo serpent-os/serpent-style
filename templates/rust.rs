32743ec0f6f9753fbbf480c16bde5f4547943189
Rune Morling <ermo@serpentos.com>
2023-09-05 00:44:41 +0200

    rust.rs: Fix rust licence template
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- templates/rust.rs
+++ templates/rust.rs
@@ -1,7 +1,5 @@
-/*
- * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
- *
- * SPDX-License-Identifier: MPL-2.0
- */
+// SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
 
-mod fixme;
+
